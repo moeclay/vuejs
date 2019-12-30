9177533a153edfc1ab8e5f5987bf09a300cdf8cc
Doctor moeclay <moeamarreza@gmail.com>
2019-12-30 13:53:53 +0700

    Create template1.js

---
+++ js/template1.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+    el: '#vue_det',
+    data: {
+        firstname: "Ria",
+        lastname: "Singh",
+        htmlcontent: "<div><h1>Vue JS Templating</h1></div>",
+        imgsrc: "logo.png"
+    }
+});
